1c1795fc3c748d40d16cb14e90fe697d58879457
aftermath1302 <saiyamkumar13022006@gmail.com>
2026-09-12 23:23:00 +0530

    newer updates

--- typeException.java
+++ typeException.java
@@ -1,8 +1,37 @@
+// Types of exception :
+// 1. Compile time error  -> occcurs during compilation of code if the file is missing or some like that
+// 2. Runtime error -> occurs during the running of code , In this the code may break at anytime
+// 3. Logical error -> the expected outcome is much weirer than the latest 
+// Types of statements :
+// 1. Normal 
+// 2. Critical [we write exception for these]
+
+class SaiyamException extends Exception{                      //in order to make a custom exception we need to and must extend exception class
+    public SaiyamException(String str) {           
+        super(str);                                           //sending the str to super class the super class has its own method to display the error
+    }
+}
+
 public class typeException {
     public static void main(String a[]) {
-        // Types of exception :
-        // 1. Compile time error  -> occcurs during compilation of code if the file is missing or some like that
-        // 2. Runtime error -> occurs during the running of code , In this the code may break at anytime
-        // 3. Logical error -> the expected outcome is much weirer than the latest 
+        int i = 0;
+        int j = 0;
+        int[] arr = new int[5];
+        try {
+            // j = i/j;                                      //these two are critical statements here
+            j = arr[5];
+            if (j==0) {
+                throw new SaiyamException("the result can not be zero");
+            }
+        } 
+        catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
