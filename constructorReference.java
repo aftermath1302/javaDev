17ba426bdd2486c6c6a2008ab707d74167d0fb0f
aftermath1302 <saiyamkumar13022006@gmail.com>
2026-09-18 23:49:34 +0530

    newer

---
+++ constructorReference.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+
+    }
+    
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + " , age=" + age + "]";
+    }
+}
+
+public class constructorReference {
+    public static void main(String a[]) {
+        List<String> names = Arrays.asList("Saiyam", "Mudgal", "Shreeya");
+        
+        List<Student> students = new ArrayList<>();
+        
+        // for (String name : names) {
+        //     students.add(new Student(name));
+        // }
+
+        students = names.stream()
+                        // .map(name -> new Student(name))
+                        .map(Student::new)
+                        .toList();
+
+        System.out.println(students);
+    }
+}
